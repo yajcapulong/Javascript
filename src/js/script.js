e63d38bdd4321c622300411f2b43b5ba0c010196
okeneru <yaj.cplng@gmail.com>
2023-05-21 21:29:43 +0800

    Javascript exercises

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,6 @@ function chars(){
     document.getElementById("demo1").innerHTML = getChar;
 }
 
-// generate a function that will return the last character of a string
 function lastChar(){
     let text = "Hatdog";
     let lastChar = text.charAt(text.length-1);
@@ -28,5 +27,35 @@ function lastChar(){
 function reversePhrase(){
     let text = document.getElementById("demo1").innerHTML;
     let splitText = text.split(" ").map((item) => item.split("").reverse().join("")).join(" ");
+
     return document.getElementById("demo2").innerHTML = splitText;
+}
+
+// find the largest number in an array
+function largestNumber(){
+    let numArray = [10, 7, 5, 8, 25, 19];
+    let largestNum = Math.max(...numArray);
+    document.getElementById("demo1").innerHTML = largestNum;
+}
+
+//Word Count
+function wordCount(){
+    let text = document.getElementById("demo1").innerHTML;
+    let splitText = text.split(/[ ,]+/);
+    let wordCount = {};
+    
+    splitText.forEach(function(word) {
+        if (wordCount[word]){
+            wordCount[word] ++;
+        }else {
+            wordCount[word] = 1;
+        }
+    });
+    const demo2 = document.getElementById("demo2");
+    for (const word in wordCount) {
+        const count = wordCount[word];
+        const wordCountElement = document.createElement("p");
+        wordCountElement.textContent = `${word}: ${count}`;
+        demo2.appendChild(wordCountElement);
+      }
 }
