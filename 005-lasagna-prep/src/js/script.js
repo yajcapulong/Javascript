869562a934b6713f4b091aeb70da8ad001983983
okeneru <yaj.cplng@gmail.com>
2023-05-25 23:03:51 +0800

    Updated changes in function

--- 005-lasagna-prep/src/js/script.js
+++ 005-lasagna-prep/src/js/script.js
@@ -1,30 +1,24 @@
-console.log("Script file loaded.");
-
 const EXPECTED_MINUTES_IN_OVEN = 40;
 const MINUTES_PER_LAYER = 2;
 
 function remainingMinsInOven(actualMinutesInOven){
-    let result1 = EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
-    document.getElementById("result1").textContent = result1;
+    let minsInOven = parseFloat(document.getElementById("actualMinutesInOven").value);
+    let result1 = EXPECTED_MINUTES_IN_OVEN - minsInOven;
+    document.getElementById("result1").textContent = "The remaining time in the oven for the lasagna is: " + result1;
 }
 
 function prepTimeInMins(numOfLayers){
-    let result2 =  MINUTES_PER_LAYER * numOfLayers;
-    document.getElementById("result2").textContent = result2;
+    let numLayers = parseFloat(document.getElementById("numOfLayers").value);
+    let result2 =  MINUTES_PER_LAYER * numLayers;
+    document.getElementById("result2").textContent = "The preparation time for a lasagna with " + numLayers + " layers is: " + result2;
 }
 
 function totalTimeInMins(numOfLayers, actualMinutesInOven){
-    let result3 =  prepTimeInMins(numOfLayers) + actualMinutesInOven;
+    let numberOfLayers = parseFloat(document.getElementById("numOfLayers").value);
+    let prepTime = prepTimeInMins(numberOfLayers);
+    let actualMinsInOven = parseFloat(document.getElementById("actualMinutesInOven").value);
+    let result3 =  prepTime + actualMinsInOven;
     document.getElementById("result3").textContent = result3;
+    
 }
 
-// Assuming you have a button with the ID "calculateButton" to trigger the calculations
-let calculateButton = document.getElementById("calculateButton");
-calculateButton.addEventListener("click", function() {
-    let actualMinutesInOven = parseInt(document.getElementById("actualMinutesInOven").value);
-    let numOfLayers = parseInt(document.getElementById("numOfLayers").value);
-
-    remainingMinsInOven(actualMinutesInOven);
-    prepTimeInMins(numOfLayers);
-    totalTimeInMins(numOfLayers, actualMinutesInOven);
-});
