8e8425c48d129fdebdbb3ac95484bdc9e07a640e
okeneru <yaj.cplng@gmail.com>
2023-05-25 23:42:15 +0800

    Fixed the function
    Co-authored-by: Noey Ignacio <hello@noeyislearning.dev>

--- 005-lasagna-prep/src/js/script.js
+++ 005-lasagna-prep/src/js/script.js
@@ -1,24 +1,32 @@
 const EXPECTED_MINUTES_IN_OVEN = 40;
 const MINUTES_PER_LAYER = 2;
 
-function remainingMinsInOven(actualMinutesInOven){
+function remainingMinsInOven(){
     let minsInOven = parseFloat(document.getElementById("actualMinutesInOven").value);
     let result1 = EXPECTED_MINUTES_IN_OVEN - minsInOven;
     document.getElementById("result1").textContent = "The remaining time in the oven for the lasagna is: " + result1;
+    return result1;
 }
 
-function prepTimeInMins(numOfLayers){
+function prepTimeInMins() {
     let numLayers = parseFloat(document.getElementById("numOfLayers").value);
-    let result2 =  MINUTES_PER_LAYER * numLayers;
+    console.log(`NumLayers:` + numLayers);
+    let result2 = MINUTES_PER_LAYER * numLayers;
     document.getElementById("result2").textContent = "The preparation time for a lasagna with " + numLayers + " layers is: " + result2;
+    return result2;
 }
-
-function totalTimeInMins(numOfLayers, actualMinutesInOven){
-    let numberOfLayers = parseFloat(document.getElementById("numOfLayers").value);
-    let prepTime = prepTimeInMins(numberOfLayers);
-    let actualMinsInOven = parseFloat(document.getElementById("actualMinutesInOven").value);
-    let result3 =  prepTime + actualMinsInOven;
-    document.getElementById("result3").textContent = result3;
-    
-}
+  
+function totalTimeInMins() {
+    let numLayers = parseFloat(document.getElementById("numOfLayers").value);
+    console.log(`NumLayers:` + numLayers);
+    let minsInOven = parseFloat(document.getElementById("actualMinutesInOven").value);
+  
+    let prepTime = prepTimeInMins(numLayers);
+    let result3 = prepTime + minsInOven;
+    console.log(minsInOven);
+    console.log(`Preptime:` + prepTime);
+  
+    document.getElementById("result3").textContent = "The total time for the lasagna is: " + result3 + " minutes";
+  }
+  
 
